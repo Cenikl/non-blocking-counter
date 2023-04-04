cb1ac63ba6fd65508992e30748804f62363ba71e
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-04-04 14:36:52 +0300

    chore: correction

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,25 @@
+import { useState, useEffect } from "react";
+
 const Counter = ({from, to}) => {
+    const progressivelyPrintBy = 100;
+    const [maxPrinted, setMaxPrinted] = useState(from + progressivelyPrintBy);
 
-///Generating data for the table
-const divideInto = 100;                                       
-const chunkSize = to/divideInto;  
-let consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-let iteration = 0;  
-
-setTimeout(function generateRows(){
-  for(from ; from <= to+100; to++ ){
-   consecutiveNumbers = [...Array(chunkSize).keys()].map(i => i + from);
-  }
-  iteration++;
-  if (iteration < divideInto)
-    setTimeout(generateRows, 0);
-},0);
-
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
+    useEffect(() => {
+        const progressivelyIncreaseMaxPrinted = currentMaxPrinted => {
+            if(currentMaxPrinted < to){
+                setMaxPrinted(currentMaxPrinted + progressivelyPrintBy);
+                setTimeout(() => progressivelyIncreaseMaxPrinted(currentMaxPrinted + progressivelyPrintBy),0);
+            } else {
+                setMaxPrinted(to);
+            }
+        };
+        progressivelyIncreaseMaxPrinted(maxPrinted);
+    }, []);
+    
+    const consecutiveNumbers = [...Array(maxPrinted-from).keys()].map(i => i + from);
+    return <div>
+        {consecutiveNumbers.map(n => <p key={n}>{n}</p>)}
+        </div> 
 }
 
-export default Counter;
+export default Counter
